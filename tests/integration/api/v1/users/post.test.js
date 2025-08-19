6035d48797799ee11b33df46c4bccaacd6475eb7
henrygoncalvess <octanebt@gmail.com>
2025-08-19 19:50:17 -0300

    feat: add support for `PATCH` on `/api/v1/users/[username]`

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -90,7 +90,7 @@ describe("POST api/v1/users", () => {
       expect(response2Body).toEqual({
         name: "ValidationError",
         message: "O email informado já está sendo utilizado.",
-        action: "Utilize outro email para realizar o cadastro.",
+        action: "Utilize outro email para realizar esta operação.",
         status_code: 400,
       });
     });
@@ -129,7 +129,7 @@ describe("POST api/v1/users", () => {
       expect(response2Body).toEqual({
         name: "ValidationError",
         message: "O apelido informado já está sendo utilizado.",
-        action: "Utilize outro apelido para realizar o cadastro.",
+        action: "Utilize outro apelido para realizar esta operação.",
         status_code: 400,
       });
     });
